e9ec33e64cc22e9f81a566eddadfb1ba60cf3543
codingQing <2649199803@qq.com>
2020-08-21 16:23:45 +0800

    第一次提交

---
+++ HelloGit/src/main/java/com/atguigu/git/HelloGitWorld.java
@@ -0,0 +1,12 @@
+package com.atguigu.git;
+
+/**
+ * @author qy
+ * @date 2020/8/21 -16:11
+ */
+public class HelloGitWorld {
+    public void say(String name){
+        System.out.println("Hello"+name);
+    }
+
+}
